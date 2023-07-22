558234f2ebc9e95ea9d5348f854ed8b64484a205
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-22 12:29:32 +0000

    fixing run

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+// Declare Functions
+void printMessage (void);
+
 int main(void) {
-  printf("Hello World\n");
+
+  // Declare variables
+  int i;
+
+  // Loop five times
+  for (i = 1; i <= 5; i++) {
+    printMessage ();
+  }
+  
   return 0;
+}
+
+// Define Functions
+void printMesage (void) {
+  printf ("Programming is Fun!\n");
 }
